1d34839b6a716c8980df1b4dfad7ef4af0390f99
nodelink <kachalovd@banking-finance.net>
2014-11-03 14:49:42 +0300

    My First Angular.js version!

---
+++ app/app.js
@@ -0,0 +1,21 @@
+function StoreController(){
+alert('Welcome, Rembo!')
+}
+
+//var app = angular.module('store', [ ]);
+
+            // A closure:
+(function(){var app = angular.module('store', [ ]);
+
+            // controller:
+    app.controller('StoreController', function(){
+    this.product = gem;});
+
+            // object to print to the page:
+    var gem = {
+        name: 'Dodecahedron',
+        price: 2.95,
+        description: 'If a fiddle is loaded clicking on Update will create a new version of the fiddle and load it into the browser. Its URL will now contain a version number. This and former versions are still available.',
+}
+
+})();
